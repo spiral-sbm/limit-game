01354be77253679641cd1722e78bf5f29ccb33b6
Felipe Samuel <129466661+felipe-sbm@users.noreply.github.com>
2024-04-20 23:43:08 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,19 +18,19 @@ function draw() {
   fill('black')
   text(debugText, 10, 20);
 
-  if (mouseX >= 150 && mouseX <= 250 && mouseY >=185 && mouseY <= 215) {
-    fill('yellow');
-    rect (200, 200, 100, 30);
-  } if (mouseX >= 150 && mouseX <= 250 && mouseY >= 225 && mouseY <= 255) {
-    fill('blue');
-    rect (200, 240, 100, 30);
-  } if (mouseX >= 160 && mouseX < 240 && mouseY >= 265 && mouseY <= 295) {
-    fill('blue');
-    rect(200, 280, 80, 30);
-  }
-
   fill('green');
   rect (200, 280, 80, 30);
+
+    if (mouseX >= 150 && mouseX <= 250 && mouseY >= 185 && mouseY <= 215) {
+      fill('yellow');
+      rect (200, 200, 100, 30);
+  } if (mouseX >= 150 && mouseX <= 250 && mouseY >= 225 && mouseY <= 255) {
+      fill('blue');
+      rect (200, 240, 100, 30);
+  } if (mouseX >= 160 && mouseX <= 240 && mouseY >= 265 && mouseY <= 295) {
+      fill('blue');
+      rect(200, 280, 80, 30);
+  }
 }
 function mouseClicked() {
 console. log("hey, listen!");
