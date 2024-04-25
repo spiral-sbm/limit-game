de531549739828bfea92d2b655f9e0e48b9bd7f8
Felipe Samuel <129466661+felipe-sbm@users.noreply.github.com>
2024-04-25 15:12:22 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,71 +7,104 @@ const CREDITS = "credits";
 
 var TELA = MENU;
 let img;
+let myFont;
+
+function preload() {
+  myFont = loadFont('assets/Rock3D-Regular.ttf'); 
+}
 
 function setup() {
-  createCanvas(640, 480);
-  textFont("Courier New");
-  img = loadImage(
-    "https://th.bing.com/th/id/OIP.81TzTzIYDeR-e1WkIkS6gwHaHa?rs=1&pid=ImgDetMain"
-  );
+  createCanvas(640, 480, WEBGL);
+  textFont(myFont);
+  img = loadImage('assets/samuel.webp');
 }
 
 function draw() {
   // Posição dos elementos centralizada.
 
   rectMode(CENTER);
+  textFont(myFont); 
 
   // Posição do mouse na tela (debug).
   fill('white')
   let debugText = "X: " + mouseX + "\nY: " + mouseY;
-  background(5);
+  background(500);
 
   // Botões.
 
-  fill("blue");
-  rect(320, 220, 300, 30);
-
-  fill("yellow");
-  rect(320, 260, 300, 30);
-
-  fill("black");
-  text(debugText, 10, 20);
-
-  fill("green");
-  rect(320, 300, 300, 30);
+  fill('black');
+  rect(0, 0, 300, 30);
+  rect(0, 50, 300, 30);
+  rect(0, 100, 300, 30);
+  
+  textSize(20);
+  text(debugText, -300, -200);
+  textSize(50);
+  text('Limit.', -90, -75);
+  textSize(20);
+  text('An empty bliss beyond this game.', -230, -50)
+  
+  fill('white');
+  text('Jogar', -35, 5);
+  text('Controles', -50, 55);
+  text('Creditos', -45, 105);
 
   // Verificador de botões.
 
-  if (mouseX >= 150 && mouseX <= 470 && mouseY >= 205 && mouseY <= 235) {
+  if (mouseX >= 170 && mouseX <= 470 && mouseY >= 225 && mouseY <= 255) {
+    background(5);
     fill('white');
-    rect(320, 220, 300, 30);
+    rect(0, 0, 300, 30);
+    textSize(50);
+    text('Limit.', -90, -75);
+    textSize(20);
+    text('An empty bliss beyond this game.', -230, -50)
+    fill('black');
+    text('JOGAR!?', -55, 5);
   }
-  if (mouseX >= 150 && mouseX <= 470 && mouseY >= 245 && mouseY <= 275) {
+  if (mouseX >= 170 && mouseX <= 470 && mouseY >= 275 && mouseY <= 305) {
+    background(5);
     fill('white');
-    rect(320, 260, 300, 30);
+    rect(0, 50, 300, 30);
+    textSize(50);
+    text('Limit.', -90, -75);
+    textSize(20);
+    text('An empty bliss beyond this game.', -230, -50)
+    fill('black');
+    text('CONTROLES!?', -75, 55);
   }
-  if (mouseX >= 160 && mouseX <= 470 && mouseY >= 285 && mouseY <= 315) {
+  if (mouseX >= 170 && mouseX <= 470 && mouseY >= 325 && mouseY <= 355) {
+    background(5);
     fill('white');
-    rect(320, 300, 300, 30);
+    rect(0, 100, 300, 30);
+    textSize(50);
+    text('Limit.', -90, -75);
+    textSize(20);
+    text('An empty bliss beyond this game.', -230, -50)
+    fill('black');
+    text('CREDITOS!?', -65, 105);
   }
 
   // Verificador de telas.
 
   if (TELA === CONTROLS) {
+    background(5);
     text("Essa é a tela de controles", 100, 100);
   }
   if (TELA === CREDITS) {
-    text("Essa é a tela de créditos", 100, 100);
-    fill("gray");
-    rect(200, 200, 300, 250);
-    image(img, 170, 135, 50, 50);
-    text("Felipe SBM");
+    background(5);
+    fill('white');
+    rect(0, 0, 433, 320);
+    image(img, 0, 0, 50, 50);
+    fill('black');
+    text('Felipe SBM', 0, 0);
+    text('Heyyyyy teste numero 1.', 0, 0);
   }
 }
 function mouseClicked() {
   // Verificador de click em botões.
 
-  if (mouseX >= 160 && mouseX <= 470 && mouseY >= 265 && mouseY <= 295) {
+  if (mouseX >= 170 && mouseX <= 470 && mouseY >= 325 && mouseY <= 355) {
     console.log("Você clicou!");
     if (TELA === MENU) {
       TELA = CREDITS; // Verificador de telas.
