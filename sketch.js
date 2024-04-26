4040bfa12e8cba3e2a60868bb7bace96d1788a9b
Felipe Samuel <129466661+felipe-sbm@users.noreply.github.com>
2024-04-25 23:02:32 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,6 +7,7 @@ const CREDITS = "credits";
 
 var TELA = MENU;
 let img;
+let img2;
 let sbm;
 let myFont;
 
@@ -19,6 +20,7 @@ function setup() {
   textFont(myFont);
   img = loadImage("assets/AEBBTW.webp");
   sbm = loadImage("assets/samuel.webp");
+  img2 = loadImage("assets/AEBBTW2.webp");
 }
 
 function draw() {
@@ -55,7 +57,7 @@ function draw() {
 
   if (
     mouseX >= 245 &&
-    mouseX <= 395 &&
+    mouseX <= 393 &&
     mouseY >= 225 &&
     mouseY <= 255 &&
     TELA !== CREDITS &&
@@ -73,23 +75,23 @@ function draw() {
     text("JOGAR!?", -55, 5);
 
     noStroke();
-    fill(50);
+    fill(25);
     push();
     translate(-275, 175);
     rotateY(1.25);
     rotateX(-0.9);
-    box(100);
+    box(200);
     pop();
     noFill();
     stroke(255);
     push();
-    translate(500, height * 0.35, -200);
-    sphere(300);
+    translate(500, height * 0.25, -200);
+    sphere(350);
     pop();
   }
   if (
     mouseX >= 245 &&
-    mouseX <= 395 &&
+    mouseX <= 393 &&
     mouseY >= 275 &&
     mouseY <= 305 &&
     TELA !== CREDITS &&
@@ -107,6 +109,15 @@ function draw() {
     text("CONTROLES!?", -85, 55);
 
     noStroke();
+    fill(75);
+    push();
+    translate(-75, -5, -75);
+    rotateY(-3.14);
+    rotateX(-15);
+    box(150);
+    pop();
+    
+    noStroke();
     fill(50);
     push();
     translate(-275, 175);
@@ -114,10 +125,19 @@ function draw() {
     rotateX(-0.9);
     box(100);
     pop();
+    
+    noStroke();
+    fill(25);
+    push();
+    translate(-75, 195);
+    rotateY(2.75);
+    rotateX(-2);
+    box(50);
+    pop();
   }
   if (
     mouseX >= 245 &&
-    mouseX <= 395 &&
+    mouseX <= 393 &&
     mouseY >= 325 &&
     mouseY <= 355 &&
     TELA !== CREDITS &&
@@ -133,26 +153,34 @@ function draw() {
     text("An empty bliss beyond this game.", -230, -50);
     fill("black");
     text("CREDITOS!?", -75, 105);
-
-    noFill();
-    stroke(255);
+    
+    ambientLight(60, 60, 60);
+    pointLight(255, 255, 255, mouseX, mouseY, 100);
     push();
-    translate(500, height * 0.35, -200);
-    sphere(300);
+    translate(-75, -50, -250);
+    rotateZ(frameCount * 0.001);
+    rotateX(frameCount * 0.001);
+    rotateY(frameCount * 0.001);
+    texture(img2);
+    box(300);
     pop();
   }
 
   // Verificador de telas.
-
+  if (TELA === GAME) {
+    background(5);
+    fill('white');
+    text("Em desenvolvimento..", -120, 0);
+  }
   if (TELA === CONTROLS) {
     background(5);
-    text("Essa é a tela de controles", 100, 100);
+    text("Only Jesus is no controle, se vira.", -230, 0);
   }
   if (TELA === CREDITS) {
     background(5);
     fill("white");
     rect(0, 0, 600, 440);
-    stroke(0);
+    stroke(0); // Fundo preto e tela branca, causando efeito de borda.
 
     image(sbm, -280, -200, 250, 250);
     fill("black");
@@ -169,7 +197,8 @@ function draw() {
     text("felipe.sbm@icloud.com", -280, 120);
     rect(-155, 170, 250, 60);
     fill('white');
-    text("VOLTAR!", -225, 180);
+    text("VOLTAR!", -210, 180); // Créditos do jogo e informações básicas.
+    
     if (mouseX >= 40 && mouseX <= 290 && mouseY >= 380 && mouseY <= 440) {
       background(13);
       fill(RGB, 5, 5, 5);
@@ -180,13 +209,28 @@ function draw() {
       fill("white");
       text("VOLTAR? OQUE E ISSO?", -200, -170);
       image(img, -280, -115, 255, 255);
-      text("??????", -205, 180);
+      text("??????", -190, 180);
     }
   }
 }
 function mouseClicked() {
   // Verificador de click em botões.
-
+  if (
+    mouseX >= 245 &&
+    mouseX <= 393 &&
+    mouseY >= 225 &&
+    mouseY <= 255  
+  ) {
+    TELA = GAME;
+  }
+  if (
+    mouseX >= 245 &&
+    mouseX <= 393 &&
+    mouseY >= 275 &&
+    mouseY <= 305  
+  ) {
+    TELA = CONTROLS;
+  }
   if (mouseX >= 170 && mouseX <= 470 && mouseY >= 325 && mouseY <= 355) {
     if (TELA === MENU) {
       TELA = CREDITS; // Verificador de telas.
