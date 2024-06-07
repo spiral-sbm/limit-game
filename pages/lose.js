ae7b6784530288b1a7514353f152d9f85c969850
Felipe Samuel <129466661+felipe-sbm@users.noreply.github.com>
2024-06-06 22:57:56 -0300

    Update lose.js

--- pages/lose.js
+++ pages/lose.js
@@ -21,36 +21,34 @@ function drawLose() {
   fill("yellow");
   text("Voltar ao menu", 250, 325);
   pop();
-
+  
   push();
   stroke("white");
   fill("black");
   rect(320, 50, 450, 60);
   pop();
   pop();
-
+  
   push();
   textFont(prpg);
   textSize(12);
   fill("white");
-  text(
-    "Dicas: O personagem e imortal, mas precisa defender sua casa ao lado direito. Alguns inimigos podem ser mais fortes que os outros, tome cuidado. Se conseguir mais de 50 rublux ganha o limit.",
-    325,
-    40,
-    450,
-  );
+  text("Dicas: O personagem e imortal, mas precisa defender sua casa ao lado direito. Alguns inimigos podem ser mais fortes que os outros, tome cuidado. Se conseguir mais de 50 rublux ganha o limit.", 325, 40, 450);
   pop();
+  
+  image(knife, 525, 60, 35, 35)
+  image(heart, 85, 5, 25, 25)
 }
 
 function goingBack() {
   window.history.back();
   setTimeout(() => {
-    window.location.reload();
+  window.location.reload();
   }, 100);
 }
 
 function onLoseClick() {
   if (TELA === LOSE) {
-    buttonClick(320, 320, 180, 30, () => goingBack());
+    buttonClick(320, 320, 180, 30, () => (goingBack()));
   }
 }
