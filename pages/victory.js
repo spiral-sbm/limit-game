1830f8fdec0ba71c96715cd7342eaba9cab94399
Felipe Samuel <129466661+felipe-sbm@users.noreply.github.com>
2024-06-07 23:55:48 -0300

    Update victory.js

--- pages/victory.js
+++ pages/victory.js
@@ -14,18 +14,16 @@ function drawWin() {
   textFont(prpg);
   textSize(18);
   fill(235);
-  let score = "Pontos totais: " + cash;
-  text(score, 240, 250);
-  stroke("yellow");
-  drawButton(320, 320, 210, 30, "red");
-  fill("yellow");
-  text("Voltar ao menu", 250, 325);
+  stroke("white");
+  drawButton(200, 400, 210, 30, "black");
+  fill("white");
+  text("Voltar ao Limit.", 120, 407);
   pop();
   
   push();
   stroke("white");
   fill("black");
-  rect(320, 50, 450, 60);
+  rect(323, 450, 455, 60);
   pop();
   pop();
   
@@ -33,15 +31,19 @@ function drawWin() {
   textFont(prpg);
   textSize(12);
   fill("white");
-  text("Dicas: O personagem e imortal, mas precisa defender sua casa ao lado direito. Alguns inimigos podem ter diferentes pontos de dano, tome cuidado. Cada fase e concluida ao conseguir 15 rublux.", 325, 40, 450);
+  let score = " Total de Rublux conseguidos: " + cash;
+  text("Congratulacoes. Voce chegou ao Limite, nao ha mais nada a ser feito, voce acabou com todos... voce chegou em um lugar que almeija inveja Acabou."+ score, 325, 440, 450);
   pop();
   
-  image(rublux, 525, 60, 25, 25)
-  image(rock, 85, 5, 25, 25)
+  
+  character.display();
+  character.move();
+  image(rublux, 535, 405, 25, 25);
+  image(rock, 80, 460, 25, 25);
 }
 
 function onWinClick() {
   if (TELA === LOSE) {
-    buttonClick(320, 320, 180, 30, () => (goingBack()));
+    buttonClick(200, 400, 180, 30, () => (TELA = MENU));
   }
 }
