33b3705e9aac7911497061a8cd0187191f9da366
Felipe Samuel <129466661+felipe-sbm@users.noreply.github.com>
2024-06-09 15:36:57 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,10 @@
-//Criando o menu do jogo, parte 10
+// Bem-vindo ao Limit, An Empty Bliss Beyond This Game.
+// Este jogo não foi utilizado Inteligências Artificiais!!! #odeioIA.
+// Artes feitas pelo pintor e desenhista potiguar Pedro Emanuel.
+// Jogo indie, artista pequeno e sem muita experiência.
+// E a mecânica de inimigos extraída de um criador de conteúdo de poucos assinantes.
 
+// Telas do jogo.
 const MENU = "menu";
 const GAME = "game";
 const CONTROLS = "controls";
@@ -7,7 +12,7 @@ const CREDITS = "credits";
 const LOSE = "lose";
 const VICTORY = "victory";
 
-var TELA = MENU;
+var TELA = MENU; // Tela que iniciará.
 
 // Imagens do menu, controles e créditos, respectivamente.
 let menu;
@@ -15,6 +20,11 @@ let spiral;
 let fundo;
 let mouse;
 let arrows;
+let sbm;
+let die;
+let jul;
+let mrw;
+let pdr;
 
 // Imagens do jogo.
 let character;
@@ -24,6 +34,12 @@ let knife;
 let heart;
 let rublux;
 
+// Sons do jogo.
+let limit;
+let victory;
+let died;
+let throwing;
+
 // Variáveis responsáveis pelo funcionamento do jogo
 let inimigo;
 let life = 3;
@@ -31,8 +47,9 @@ let cash = 0;
 let bullets = []; // Fessô, são armas de brinquedo, não me cancele!
 let enemies = [];
 let aceleration = 1.0;
-let initial = -3000;
-let verify = false;
+let initial = -1000;
+let win = false;
+let verify;
 
 // Variáveis responsáveis por fontes e cor.
 let myFont;
@@ -40,15 +57,22 @@ let prpg;
 let BG = 500;
 
 function preload() {
+  // Fontes usadas no jogo.
   myFont = loadFont("public/Rock3D-Regular.ttf");
   prpg = loadFont("public/Propaganda.ttf");
+
+  // Sons do jogo
+  limit = loadSound("public/limit.mp3", carregado);
+  victory = loadSound("public/win.mp3");
+  died = loadSound("public/die.mp3");
+  throwing = loadSound("public/shoot.mp3");
 }
 
 function setup() {
   createCanvas(640, 480); // Qualidade do jogo.
   textFont(myFont); // Fonte do Limit.
   textFont(prpg); // Fonte geral.
-  angleMode(DEGREES);
+  angleMode(DEGREES); // Girar ângulo em graus.
 
   retry = createButton("retry");
   retry.hide();
@@ -59,6 +83,11 @@ function setup() {
   menu = loadImage("public/ivan-menu.webp");
   spiral = loadImage("public/spiral-games.webp");
   fundo = loadImage("public/background.webp");
+  sbm = loadImage("public/sbm.webp");
+  die = loadImage("public/die.webp");
+  jul = loadImage("public/julians.webp");
+  mrw = loadImage("public/mrw.webp");
+  pdr = loadImage("public/pdr.webp");
 
   character = new Character("public/ivan.webp", width / 2, height / 2);
   frame = loadImage("public/frame.webp");
@@ -68,13 +97,19 @@ function setup() {
   rublux = loadImage("public/rublux.webp");
   inimigo = loadImage("public/enemy.webp");
 
-  for (let i = 0; i < 25; i++) {
+  // Carregar inimigos.
+  for (let i = 0; i < 15; i++) {
     let enemy = {
       x: random(-640, 0),
       y: random(40, 400),
     };
     enemies.push(enemy);
   }
+}
+
+// Função para carregar música em loop
+function carregado() {
+  limit.loop(); // Música feita por mim.
 }
 
 // Impressão das telas do jogo.
@@ -95,9 +130,10 @@ function draw() {
     drawControls();
   }
   if (TELA === CREDITS) {
-    BG = 15;
+    BG = 255;
     drawCredits();
-  } if (TELA === LOSE) {
+  }
+  if (TELA === LOSE) {
     BG = 0;
     drawLose();
   } else if (TELA === VICTORY) {
