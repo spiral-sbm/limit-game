0cbc0f58868a014d46dbc4259299644fcb6fb558
Felipe Samuel <129466661+felipe-sbm@users.noreply.github.com>
2024-06-08 23:36:11 -0300

    Update lose.js

--- pages/lose.js
+++ pages/lose.js
@@ -2,6 +2,8 @@
 function drawLose() {
   push();
 
+  cursor("progress");
+
   push();
   textFont(myFont);
   textAlign(CENTER);
@@ -21,27 +23,32 @@ function drawLose() {
   fill("yellow");
   text("Voltar ao menu", 250, 325);
   pop();
-  
+
   push();
   stroke("white");
   fill("black");
   rect(320, 50, 450, 60);
   pop();
   pop();
-  
+
   push();
   textFont(prpg);
   textSize(12);
   fill("white");
-  text("Dicas: O personagem e imortal, mas precisa defender sua casa ao lado direito. Alguns inimigos podem ter diferentes pontos de dano, tome cuidado. Cada fase e concluida ao conseguir 15 rublux.", 325, 40, 450);
+  text(
+    "Dicas: O personagem e imortal, mas precisa defender sua casa ao lado direito. Cada fase e ultrapassada ao conseguir certa quantidade de Rublux. No total existem 4 fases, boa sorte...",
+    325,
+    40,
+    450,
+  );
   pop();
-  
-  image(knife, 525, 60, 35, 35)
-  image(heart, 85, 5, 25, 25)
+
+  image(knife, 525, 60, 35, 35);
+  image(heart, 85, 5, 25, 25);
 }
 
 function onLoseClick() {
   if (TELA === LOSE) {
-    buttonClick(320, 320, 180, 30, () => (goingBack()));
+    buttonClick(320, 320, 180, 30, () => goingBack());
   }
 }
