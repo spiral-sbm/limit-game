b3e5666e2cd086f8876173c616504606ad0b41b9
Felipe Samuel <129466661+felipe-sbm@users.noreply.github.com>
2024-06-09 15:57:15 -0300

    menu.js Finalizado.

--- pages/menu.js
+++ pages/menu.js
@@ -1,16 +1,18 @@
 function drawMenu() {
   BG = 5;
   background(menu);
+  cursor("grab"); // Cursor com mãozinha.
 
+  // Barra de botões
   push();
   fill("black");
   rect(320, 465, 640, 35);
-  pop();
-
   drawButton(100, 450, 150, 30, "black");
   drawButton(320, 450, 150, 30, "black");
   drawButton(540, 450, 150, 30, "black");
+  pop();
 
+  // Título do jogo.
   fill("white");
   push();
   stroke(0);
@@ -20,6 +22,7 @@ function drawMenu() {
   text("Limit.", 30, 120);
   pop();
 
+  // Subtítulo do jogo.
   push();
   stroke(0);
   strokeWeight(5);
@@ -29,6 +32,7 @@ function drawMenu() {
   text("An empty bliss" + "\n" + "beyond this game.", 30, 145);
   pop();
 
+  // Texto dos botões.
   push();
   textFont(prpg);
   textSize(20);
@@ -36,11 +40,14 @@ function drawMenu() {
   text("Jogar", 70, 455);
   text("Controles", 266, 455);
   text("Creditos", 495, 455);
-  image(spiral, 510, 20, 100, 50);
+  image(spiral, 510, 20, 100, 50); // Logo da Spiral.
   pop();
 }
+
+// Função de pressionar botões do menu.
 function onMenuClick() {
   if (TELA === MENU) {
+    throwing.play();
     buttonClick(100, 450, 150, 30, () => (TELA = GAME));
     buttonClick(320, 450, 150, 30, () => (TELA = CONTROLS));
     buttonClick(540, 450, 150, 30, () => (TELA = CREDITS));
