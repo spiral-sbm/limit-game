f2d8c091fc5e9a7046bda9a2a21999c1724db22f
Felipe Samuel <129466661+felipe-sbm@users.noreply.github.com>
2024-06-08 23:36:37 -0300

    Update victory.js

--- pages/victory.js
+++ pages/victory.js
@@ -2,6 +2,8 @@
 function drawWin() {
   push();
 
+  cursor("progress");
+
   push();
   textFont(myFont);
   textAlign(CENTER);
@@ -19,31 +21,37 @@ function drawWin() {
   fill("white");
   text("Voltar ao Limit.", 120, 407);
   pop();
-  
+
   push();
   stroke("white");
   fill("black");
   rect(323, 450, 455, 60);
   pop();
   pop();
-  
+
   push();
   textFont(prpg);
   textSize(12);
   fill("white");
   let score = " Total de Rublux conseguidos: " + cash;
-  text("Congratulacoes. Voce chegou ao Limite, nao ha mais nada a ser feito, voce acabou com todos... voce chegou em um lugar que almeija inveja Acabou."+ score, 325, 440, 450);
+  text(
+    "Congratulacoes. Voce chegou ao Limite, nao ha mais nada a ser feito, voce acabou com todos... voce chegou em um lugar que almeija inveja Acabou." +
+      score,
+    325,
+    440,
+    450,
+  );
   pop();
-  
-  
-  character.display();
-  character.move();
+
   image(rublux, 535, 405, 25, 25);
   image(rock, 80, 460, 25, 25);
+  character.display();
+  character.move();
 }
 
 function onWinClick() {
   if (TELA === VICTORY) {
     buttonClick(200, 400, 180, 30, () => (TELA = MENU));
+    win = true;
   }
 }
